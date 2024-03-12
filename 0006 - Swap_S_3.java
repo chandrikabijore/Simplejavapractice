e76d7bb194d993b2b09dd163ac5c5311e06b136f
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-12 23:43:56 +0530

    1203

---
+++ 0006 - Swap_S_3.java
@@ -0,0 +1,20 @@
+package com.chandrika;
+
+public class Swap_S_3
+{
+    public static void main(String args[])
+    {
+        System.out.println("Before swapping");
+        int x = 100;
+        int y = 250;
+        System.out.println("value of x : " + x);
+        System.out.println("value of y : " + y);
+
+        System.out.println("\nAfter swapping");
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        System.out.println("value of x : " + x);
+        System.out.println("value of y : " + y);
+    }
+}

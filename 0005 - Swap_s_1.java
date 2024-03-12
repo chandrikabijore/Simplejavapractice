e5b3fdc791ce3eeaaa86e6aaa2f5abb009102413
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-12 23:41:27 +0530

    1203

---
+++ 0005 - Swap_s_1.java
@@ -0,0 +1,26 @@
+package com.chandrika;
+
+import java.util.Scanner;
+
+public class Swap_s_1
+{
+    public static void main(String[] args)
+    {
+        int a, b, temp;
+        Scanner swap = new Scanner(System.in);
+
+        System.out.println("Enter number for a: ");
+        a = swap.nextInt();
+
+        System.out.println("Enter number for b : ");
+        b = swap.nextInt();
+
+        System.out.printf("Before swapping a = %d and b = %d", + a, b);
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.printf("\n\nAfter swapping a = %d and b = %d", + a,  b);
+    }
+}

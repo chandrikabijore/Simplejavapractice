bd52945a7b191fd77c60968d88ceff30660e58af
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-12 19:13:25 +0530

    1203

---
+++ 0003 - EvenOdd_s.java
@@ -0,0 +1,24 @@
+package com.chandrika;
+
+import java.util.Scanner;
+
+public class EvenOdd_s
+{
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter here any no : ");
+        n = scan.nextInt();
+
+        // r = n%2;
+         if (n % 2 == 0)
+         {
+             System.out.println(n+ " is Even");
+         }
+         else
+         {
+             System.out.println(n+ " is Odd");
+         }
+    }
+}

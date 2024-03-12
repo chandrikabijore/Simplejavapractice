706ef265576c2f4c752912129bcb27b3cb01244f
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-12 23:40:41 +0530

    1203

---
+++ 0004 - Swap_s.java
@@ -0,0 +1,20 @@
+package com.chandrika;
+
+public class Swap_s
+{
+    public static void main(String[] args)
+    {
+        int a = 10,b = 20,c = 30;
+        System.out.println("Befor swaping");
+        System.out.println("a is "+a);
+        System.out.println("b is "+b);
+
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println("after swaping");
+        System.out.println("a is "+a);
+        System.out.println("b is "+b);
+    }
+}

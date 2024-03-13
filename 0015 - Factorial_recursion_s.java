049f681f7f07e66cd4377c229f7d29b561ccd637
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-13 23:57:47 +0530

    1303

---
+++ 0015 - Factorial_recursion_s.java
@@ -0,0 +1,19 @@
+//package com.chandrika;
+//
+//public class Factorial_recursion_s
+//{
+//    public static void main(String args[]){
+//        int i, fact = 1;
+//        int number=4;//It is the number to calculate factorial
+//        fact = factorial(number);
+//        System.out.println("Factorial of "+number+" is: "+fact);
+//    }
+//    static int factorial(int n) {
+//        if (n == 0)
+//            return 1;
+//        else{
+//            return(n * factorial(n-1));
+//    }
+//}
+//
+//

42d00b113bd9635b97498f27de091b48aa00ec54
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-13 23:56:05 +0530

    1303

---
+++ 0015 - Revers_s.java
@@ -0,0 +1,13 @@
+package com.chandrika;
+
+public class Revers_s
+{
+    public static void main(String[] args)
+    {
+        int[] x = {10, 20, 30, 40, 50};
+        for (int i = x.length - 1; i >= 0; i--)
+        {
+            System.out.println(x[i]);
+        }
+    }
+}

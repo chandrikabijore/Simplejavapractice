d6a02d166d6f7a7341161d4c0015a033e597da22
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-13 22:55:57 +0530

    1303

---
+++ 0013 - Multiplication_Table_s.java
@@ -0,0 +1,19 @@
+package com.chandrika;
+
+import java.util.Scanner;
+
+public class Multiplication_Table_s
+{
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner table = new Scanner(System.in);
+        System.out.println("Enter any no to print the table : ");
+        n = table.nextInt();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.printf("%d * %d = %d\n", n, i, n * i);
+        }
+    }
+}

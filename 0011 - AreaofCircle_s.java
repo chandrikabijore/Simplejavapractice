f8c7bedb9bc756199f67ee01e24b7960e2156924
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-13 22:53:19 +0530

    1303

---
+++ 0011 - AreaofCircle_s.java
@@ -0,0 +1,19 @@
+package com.chandrika;
+
+import java.util.Scanner;
+
+public class AreaofCircle_s
+{
+    public static void main(String[] args)
+    {
+        double Area, radius;
+        Scanner circle = new Scanner(System.in);
+
+        System.out.println("Enter the radius of the circle : ");
+        radius = circle.nextDouble();
+
+        Area = Math.PI * radius * radius;
+
+        System.out.printf("The area of circle is : %f", Area);
+    }
+}

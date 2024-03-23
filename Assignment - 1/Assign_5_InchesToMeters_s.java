8426c9dd79ec6deb34031555d5cb826b69e38769
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-03-23 05:54:29 +0530

    InchesToMeters_s

---
+++ Assignment - 1/Assign_5_InchesToMeters_s.java
@@ -0,0 +1,20 @@
+// 5) Write a Java program that reads a number in inches, converts it to meters.
+package com.chandrika;
+
+import java.util.Scanner;
+
+public class Assign_5_InchesToMeters_s
+{
+    public static void main(String[] args)
+    {
+        double inches, meters;
+        Scanner In = new Scanner(System.in);
+        System.out.println("Enter the number in inches : ");      // 10
+        inches = In.nextDouble();
+
+        // Convert inches to meters (1 inch = 0.0254 meters)
+        meters = inches * 0.0254;
+
+        System.out.printf("This %f inches in meters is : %f", inches, meters);     // 0.0245000..
+    }
+}

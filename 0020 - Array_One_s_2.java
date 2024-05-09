3aa15a1791eb5d63f51b13dc4a1efb4d23d320b0
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-05-09 14:41:05 +0530

    0905_20

---
+++ 0020 - Array_One_s_2.java
@@ -0,0 +1,14 @@
+package com.chandrika;
+
+public class Array_One_s_2
+{
+    public static void main(String[] args)
+    {
+        String[] str = new String[]{"One", "Two", "Three", "Four", "Five"};
+        System.out.println("These are the Sring array elements :");
+        for (int i = 0; i < str.length; i++)
+        {
+            System.out.println("str["+i+"] : " + str[i]);
+        }
+    }
+}

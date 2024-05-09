e7f9e8b49895b684b5483c355ab6088a8f8d5d54
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-05-09 14:38:51 +0530

    0905 - 18

---
+++ 0018 - Array_One_s.java
@@ -0,0 +1,23 @@
+package com.chandrika;
+
+public class Array_One_s
+{
+    public static void main(String[] args)
+    {
+        int[] x = new int[5];
+        x[0] = 0;
+        x[1] = 1;
+        x[2] = 2;
+        x[3] = 3;
+        x[4] = 4;
+       // x[5] = 5;
+
+        System.out.println("The array elements are : ");
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);
+        System.out.println(x[3]);
+        System.out.println(x[4]);
+       // System.out.println(x[5]);
+    }
+}

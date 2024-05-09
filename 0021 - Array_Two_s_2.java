a69ca5e41244b7d66507b7feb4267c04241a5146
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-05-09 14:41:49 +0530

    0905_21

---
+++ 0021 - Array_Two_s_2.java
@@ -0,0 +1,14 @@
+package com.chandrika;
+
+public class Array_Two_s_2
+{
+    public static void main(String[] args)
+    {
+        int[][] a = {{10, 20}, {30, 40}};
+        System.out.println("Two dimensional array elements are : ");
+        System.out.println(a[0][0]);
+        System.out.println(a[0][1]);
+        System.out.println(a[1][0]);
+        System.out.println(a[1][1]);
+    }
+}

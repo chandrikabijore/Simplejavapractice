21150b0c818e0015bfe65de1efedaadb8c1246cf
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-05-09 14:40:16 +0530

    0905_19

---
+++ 0019 - Array_One_s_1.java
@@ -0,0 +1,14 @@
+package com.chandrika;
+
+public class Array_One_s_1
+{
+    public static void main(String[] args)
+    {
+        int[] a = {10, 20, 30, 40, 50};
+        System.out.println("One dimensional arrays are : ");
+        for (int i = 0; i < a.length; i++)
+        {
+            System.out.println("a["+i+"] : " + a[i]);
+        }
+    }
+}
